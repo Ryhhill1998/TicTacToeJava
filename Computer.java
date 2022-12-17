988bce84fb5f13116e8edc0125e39d640e366e96
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-12-17 16:05:38 +0000

    Add files via upload

--- Computer.java
+++ Computer.java
@@ -6,14 +6,18 @@ public class Computer extends Player {
     private static final int DEFAULT_COORDINATE = -1;
     private static final int EASY_LEVEL = 0;
     private static final int MEDIUM_LEVEL = 1;
+    private static final int HARD_LEVEL = 2;
     private static final String MARKER_X = "X";
     private static final String MARKER_O = "O";
-    int level;
+    private int level;
+    private String playerMarker;
+    private static final int infinity = (int) Double.POSITIVE_INFINITY;
 
     private String reasonForMove;
 
     public Computer(String marker, int level) {
         super(marker);
+        playerMarker = getMarker().equals("X") ? "O" : "X";
         this.level = level;
     }
 
@@ -29,6 +33,10 @@ public class Computer extends Player {
         if (level == EASY_LEVEL) {
             System.out.println("EASY LEVEL");
             return getCoordinatesEasy(board);
+        }
+
+        if (level == HARD_LEVEL) {
+            return getCoordinatesHard(board);
         }
 
         return getCoordinatesMedium(board);
@@ -80,5 +88,99 @@ public class Computer extends Player {
         }
 
         return getCoordinatesEasy(board);
+    }
+
+    public int[] getCoordinatesHard(Board board) {
+        int[] bestMove = new int[2];
+        int bestMoveVal = -infinity;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int[] coordinates = {i, j};
+                if (!board.positionIsFree(coordinates)) {
+                    continue;
+                }
+
+                board.placeMarker(getMarker(), coordinates[0], coordinates[1]);
+                int val = findBestMove(board, getMarker(), false, 0, -infinity, infinity);
+                board.placeMarker("_", coordinates[0], coordinates[1]);
+
+                if (val > bestMoveVal) {
+                    bestMoveVal = val;
+                    bestMove[0] = coordinates[0];
+                    bestMove[1] = coordinates[1];
+                }
+            }
+        }
+
+        return bestMove;
+    }
+
+    private int findBestMove(Board board, String marker, boolean maximising, int numberOfMoves, int alpha, int beta) {
+        if (maximising) {
+            if (board.markerHasWon(marker)) {
+                return -10 + numberOfMoves;
+            }
+        } else {
+            if (board.markerHasWon(marker)) {
+                return 10 - numberOfMoves;
+            }
+        }
+
+        if (board.boardIsFull()) {
+            return 0;
+        }
+
+        String currentMarker = maximising ? getMarker() : playerMarker;
+
+        if (maximising) {
+            int maxEval = -infinity;
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int[] position = {i, j};
+                    if (!board.positionIsFree(position)) {
+                        continue;
+                    }
+
+                    board.placeMarker(currentMarker, position[0], position[1]);
+                    int eval = findBestMove(board, currentMarker, false, numberOfMoves + 1, alpha, beta);
+                    board.placeMarker("_", position[0], position[1]);
+
+                    maxEval = Math.max(maxEval, eval);
+                    alpha = Math.max(alpha, eval);
+
+                    if (beta <= alpha) {
+                        return maxEval;
+                    }
+                }
+            }
+
+            return maxEval;
+        } else {
+            int minEval = infinity;
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int[] position = {i, j};
+                    if (!board.positionIsFree(position)) {
+                        continue;
+                    }
+
+                    board.placeMarker(currentMarker, position[0], position[1]);
+                    int eval = findBestMove(board, currentMarker, true, numberOfMoves + 1, alpha, beta);
+                    board.placeMarker("_", position[0], position[1]);
+
+                    minEval = Math.min(minEval, eval);
+                    beta = Math.min(beta, eval);
+
+                    if (beta <= alpha) {
+                        return minEval;
+                    }
+                }
+            }
+
+            return minEval;
+        }
     }
 }
--- ComputerIO.java
+++ ComputerIO.java
@@ -6,18 +6,18 @@ public abstract class ComputerIO {
     public static int getComputerLevel() {
         int level = -1;
 
-        while (level != 0 && level != 1) {
+        while (level != 0 && level != 1 && level != 2) {
             try {
                 Scanner scanner = new Scanner(System.in);
 
                 System.out.print("Which level computer would you like to play against? " +
-                        "Enter 0 for easy or 1 for medium: ");
+                        "Enter 0 for easy, 1 for medium or 2 for hard: ");
 
                 level = scanner.nextInt();
 
-                if (level != 0 && level != 1) {
+                if (level != 0 && level != 1 && level != 2) {
                     System.out.println();
-                    System.out.print("Invalid entry! Please enter 0 or 1: ");
+                    System.out.print("Invalid entry! Please enter 0, 1 or 2: ");
                     level = scanner.nextInt();
                 }
             } catch (InputMismatchException e) {
